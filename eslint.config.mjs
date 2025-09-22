db8c7600017005159580536e5bec628357f97500
adminCovern <brian@covrenfirm.com>
2025-09-22 12:15:38 -0700

    Update eslint.config.mjs
    eslint: disable unescaped-entities; keep strict quality rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+// eslint.config.mjs
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -9,8 +10,23 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+/**
+ * We extend Next's recommended configs, then add:
+ * - turn OFF react/no-unescaped-entities (so you don't have to escape every ' and ")
+ * - keep unused-vars STRICT, but allow names that start with "_" on purpose
+ * - keep Next internal-link rule and hooks deps rule as errors (quality, not fluff)
+ */
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      ],
+      "@next/next/no-html-link-for-pages": "error",
+      "react-hooks/exhaustive-deps": "error"
+    }
+  }
 ];
-
-export default eslintConfig; 
